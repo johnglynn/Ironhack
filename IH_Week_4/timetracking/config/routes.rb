b63e582072e3f4758835e196705ffb0cd0d16c49
john glynn <john.glynn@outlook.com>
2016-07-05 15:22:15 -0400

    second commit

--- IH_Week_4/timetracking/config/routes.rb
+++ IH_Week_4/timetracking/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+	get '/', to: 'site#home'
+	# get '/about', to: 'site#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

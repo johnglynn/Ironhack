10183695cd24b76b2ee631ddf42b47b6823d5b4a
john glynn <john.glynn@outlook.com>
2016-07-07 15:21:51 -0400

    Thursday 3rd Commit

--- IH_Week_4/timetracking/config/routes.rb
+++ IH_Week_4/timetracking/config/routes.rb
@@ -8,26 +8,9 @@ Rails.application.routes.draw do
 
 	post '/calculate', to: 'site#calculate'
 
-	get '/projects/new', to: 'projects#new'
-
-	post '/projects', to: 'projects#create'
-
-	get '/projects', to: 'projects#index'
-
-	get '/projects/:id', to: 'projects#show'
-
-
-	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
-
-	post '/projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
-
-	get '/projects/:project_id/time_entries', to: 'time_entries#index'
-
-	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
-
-	patch '/projects/:project_id/time_entries/:id', to: 'time_entries#update', as: 'project_time_entry'
-
-	delete '/projects/:project_id/time_entries/:id', to: 'time_entries#destroy'
+	resources :projects, only: [:index, :show, :new, :create] do
+		resources :time_entries, except: [:show]
+	end	
 
 	
 
